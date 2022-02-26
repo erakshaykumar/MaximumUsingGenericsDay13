f92ceedf61a1d3c34cb50870d097448a5ad0aaa3
erakshaykumar <akaakshay304@gmail.com>
2022-02-27 05:06:10 +0530

    Added: Welcome Message

---
+++ src/com/blz/maximumgenerics/Maximum.java
@@ -0,0 +1,11 @@
+/*@Purpose : Printing Welcome Message 
+ * @file : Maximum Using Generics
+ * @author : Akshay Kumar
+ */
+package com.blz.maximumgenerics;
+
+public class Maximum {
+	public static void main(String[] args) {
+		System.out.println("Welcome To The Maximum Using Generics Program");
+	}
+}
